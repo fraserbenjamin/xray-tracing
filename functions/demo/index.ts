44fd1b9fc101d6ca41581684ffd15e350c947982
Fraser Benjamin <fraser.benjamin@outlook.com>
2025-02-16 23:32:58 +0000

    Add xray example

---
+++ functions/demo/index.ts
@@ -0,0 +1,41 @@
+import { ListBucketsCommand, S3Client } from "@aws-sdk/client-s3";
+import { type Handler } from "aws-cdk-lib/aws-lambda";
+import AWSXRay from "aws-xray-sdk-core";
+
+const s3Client = AWSXRay.captureAWSv3Client(new S3Client());
+
+const wait = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+
+export const handler: Handler = async (event: any) => {
+  const segment = AWSXRay.getSegment();
+  segment?.addAnnotation("test-annotation", "global");
+  console.log(`Segment: ${segment?.name} is ${segment ? "defined" : "undefined"}`);
+
+  const subSegment = segment?.addNewSubsegment("wait");
+  subSegment?.addAttribute("test-attribute", "123");
+  subSegment?.addAnnotation("test-annotation", "456");
+  await wait(1000);
+  subSegment?.close();
+
+  const result = await AWSXRay.captureAsyncFunc("test-async", async (sub) => {
+    sub?.addAnnotation("test-annotation", "789");
+    await wait(1000);
+
+    sub?.addAnnotation("test-annotation", "abc");
+    sub?.close();
+    return "abc";
+  });
+
+  console.log(`Result: ${result}`);
+
+  const response = await s3Client.send(new ListBucketsCommand({}));
+  console.log(response);
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: "Hello from Lambda!",
+      input: event,
+    }),
+  }
+}
--- lib/xray-tracing-stack.ts
+++ lib/xray-tracing-stack.ts
@@ -1,16 +1,45 @@
 import * as cdk from 'aws-cdk-lib';
+import { Effect, PolicyStatement } from 'aws-cdk-lib/aws-iam';
+import { Architecture, Runtime, Tracing } from 'aws-cdk-lib/aws-lambda';
+import { NodejsFunction, OutputFormat } from 'aws-cdk-lib/aws-lambda-nodejs';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class XrayTracingStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const xrayTracingLambda = new NodejsFunction(this, "xray-tracing-function", {
+      functionName: "xray-tracing",
+      runtime: Runtime.NODEJS_22_X, // Provide any supported Node.js runtime
+      entry: "./functions/demo/index.ts",
+      architecture: Architecture.ARM_64,
+      timeout: cdk.Duration.seconds(10),
+      bundling: {
+        nodeModules: ["aws-xray-sdk-core"],
+        minify: false,
+        platform: 'node',
+        forceDockerBundling: false,
+        mainFields: ['module', 'main'],
+        format: OutputFormat.ESM,
+      },
+      tracing: Tracing.ACTIVE,
+      environment: {
+        AWS_XRAY_CONTEXT_MISSING: "LOG_ERROR",
+      }
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'XrayTracingQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Add the AWS X-Ray tracing policy to the Lambda function
+    xrayTracingLambda.addToRolePolicy(new PolicyStatement({
+      effect: Effect.ALLOW,
+      actions: [
+        "xray:PutTraceSegments",
+        "xray:PutTelemetryRecords",
+        "xray:GetSamplingRules",
+        "xray:GetSamplingTargets",
+        "xray:GetSamplingStatisticSummaries",
+        "s3:ListAllMyBuckets",
+      ],
+      resources: ["*"],
+    }));
   }
 }
