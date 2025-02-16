2feef12b635bee04cf9b14d4497d78cda8924f01
Fraser Benjamin <fraser.benjamin@outlook.com>
2025-02-16 21:55:00 +0000

    Initial commit

---
+++ test/xray-tracing.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as XrayTracing from '../lib/xray-tracing-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/xray-tracing-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new XrayTracing.XrayTracingStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
